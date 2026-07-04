faac01dfe3de83844ec0824558a0941f11d09cc2
sky-script <sepehr.jafarzadeh.dev@gmail.com>
2026-07-04 17:15:28 +0330

    fix: the hamburger menu didn't work because the bootstrap js bundle wasn't imported

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import 'bootstrap/dist/css/bootstrap.rtl.min.css' // Bootstrap RTL support
+import 'bootstrap/dist/js/bootstrap.bundle.min.js' // Bootstrap js bundle
 import './styles/global.css' // global overrides + fonts
 import App from './App.jsx'
 
