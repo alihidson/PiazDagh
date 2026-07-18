97f42f401d4ad422b3e1660c5fe5fe0944e5d4b0
sky-script <sepehr.jafarzadeh.dev@gmail.com>
2026-07-18 20:40:51 +0330

    fix: when the logo is clicked you'll be redirected to the root of the website

--- frontend/src/components/navbar/Logo.jsx
+++ frontend/src/components/navbar/Logo.jsx
@@ -1,11 +1,15 @@
-import logo from '../../assets/logo/PIAZDAGH-logo-main.png';
+import { Link } from "react-router-dom";
+import logo from "../../assets/logo/PIAZDAGH-logo-main.png";
 
 const Logo = ({ text_color = "text-dark" }) => {
   return (
-    <div className="d-flex align-items-center gap-4">
+    <Link
+      to="/"
+      className="d-flex align-items-center gap-4 text-decoration-none"
+    >
       <img src={logo} alt="پیاز داغ" height="60" />
       <span className={`fw-bold fs-3 ${text_color}`}>پیاز داغ</span>
-    </div>
+    </Link>
   );
 };
 
